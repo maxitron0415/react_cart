0e8df86abf8b7f0d66814d197e9f11f8a7390e8b
maxitron0415 <59229386+maxitron0415@users.noreply.github.com>
2024-04-26 14:39:20 +0300

    Add files via upload

---
+++ src/components/preloader.jsx
@@ -0,0 +1,9 @@
+function Preloader(){
+    return <div className="container">
+        <div class="progress">
+            <div class="indeterminate"></div>
+        </div>
+    </div>
+}
+
+export {Preloader};
